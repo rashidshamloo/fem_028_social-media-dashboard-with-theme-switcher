2faed162acd06a59c918d2d9e23267c2d7b5e96f
Rashid Shamloo <rashidshamloo@gmail.com>
2023-03-21 09:04:31 +0330

    Added main tag

--- src/App.vue
+++ src/App.vue
@@ -14,20 +14,22 @@ function toggleDarkMode() {
 
 <template>
   <Header :totalFollowers="23004" @toggle="toggleDarkMode()" />
-  <div
-    class="mt-11 inline-grid min-w-[88%] grid-cols-1 gap-5 sm:min-w-[33.75rem] sm:grid-cols-2 sm:gap-8 lg:min-w-full lg:grid-cols-4"
-  >
-    <SocialStat v-for="item in data" :data="item" />
-  </div>
-  <h2
-    class="mt-12 w-full max-w-[88%] text-left text-2xl font-bold text-veryDarkBlue dark:text-white lg:max-w-full"
-  >
-    Overview - Today
-  </h2>
-  <div
-    class="mx-auto mt-6 w-full grid-cols-1 gap-8 grid-areas-small sm:grid sm:w-auto lg:grid-cols-2 lg:grid-areas-large"
-  >
-    <SocialOverview v-for="item in data" :data="item" />
-  </div>
+  <main>
+    <div
+      class="mt-11 inline-grid min-w-[88%] grid-cols-1 gap-5 sm:min-w-[33.75rem] sm:grid-cols-2 sm:gap-8 lg:min-w-full lg:grid-cols-4"
+    >
+      <SocialStat v-for="item in data" :data="item" />
+    </div>
+    <h2
+      class="mt-12 w-full max-w-[88%] text-left text-2xl font-bold text-veryDarkBlue dark:text-white lg:max-w-full"
+    >
+      Overview - Today
+    </h2>
+    <div
+      class="mx-auto mt-6 w-full grid-cols-1 gap-8 grid-areas-small sm:grid sm:w-auto lg:grid-cols-2 lg:grid-areas-large"
+    >
+      <SocialOverview v-for="item in data" :data="item" />
+    </div>
+  </main>
   <Footer />
 </template>
